e9b99d7f3d46dc06d1a844fbf1de0e7ec012e3a6
mar <rojanomariano@gmail.com>
2023-01-07 18:30:13 -0800

    added comments to satrt writing code

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const inquirer = require("inquirer");
 const fs = require("fs"); 
 
-// Linking this index.js to all the files within the library 
+// Linking this index.js to all the files within the library folder
+
+
+
+
+
+// function to write HTMl file
+
+
+// Array of questions to ask for manager, engineer, intern
+
+
+// need an empty array to push these answers into 
+
+// manager questions need to come first
+// need to include github for engineer, school for intern, office number for manager

c70bed6032656f570c56c6892070463a237f06ac
mar <rojanomariano@gmail.com>
2023-02-11 05:40:14 -0800

    fixed card then copied/pasted and adjusted for employee type

--- utils/generatehtml.js
+++ utils/generatehtml.js
@@ -4,7 +4,66 @@ function generateHtml(data) {
     let htmlMarkup = "";
     for (let i = 0; i < data.length; i++){
         if (data[i].getRole() === "Intern"){
-            htmlMarkup += 
+            htmlMarkup += `
+            <div class="col-4">
+            <div class="card text-bg-dark mb-3 rounded">
+        <div class="card-body col"> 
+      <br>
+          <h5 class="card-title fw-semibold fs-4">${data[i].getRole()}</h5>
+          <hr>
+          <h6>${data[i].getName()}</h6>
+          <p class="card-text">ID: ${data[i].getId()}</p>
+          <p class="card-text">School: ${data[i].getSchool()}</p>
+          <p> Email: 
+          <a href='mailto:${data[i].getEmail()}' class="btn btn-primary">${data[
+              i
+            ].getEmail()}</a>
+            </p>
+        </div>
+        </div>
+      </div>
+     `
         }
+       if (data[i].getRole() === "Manager"){
+        htmlMarkup += `
+        <div class="col-4">
+        <div class="card text-bg-dark mb-3 rounded">
+    <div class="card-body col"> 
+  <br>
+      <h5 class="card-title fw-semibold fs-4">${data[i].getRole()}</h5>
+      <hr>
+      <h6>${data[i].getName()}</h6>
+      <p class="card-text">ID: ${data[i].getId()}</p>
+      <p class="card-text">office number: ${data[i].getOfficeNumber()}</p>
+      <p> Email: 
+      <a href='mailto:${data[i].getEmail()}' class="btn btn-primary">${data[
+          i
+        ].getEmail()}</a>
+        </p>
+    </div>
+    </div>
+  </div>
+        `
+       }
+       if (data[i].getRole() === "Engineer"){
+        htmlMarkup += `
+        <div class="col-4">
+        <div class="card text-bg-dark mb-3 rounded">
+    <div class="card-body col"> 
+  <br>
+      <h5 class="card-title fw-semibold fs-4">${data[i].getRole()}</h5>
+      <hr>
+      <h6>${data[i].getName()}</h6>
+      <p class="card-text">ID: ${data[i].getId()}</p>
+      <p> Email: 
+      <a href='mailto:${data[i].getEmail()}' class="btn btn-primary">${data[
+          i
+        ].getEmail()}</a>
+        </p>
+        <a> class="card-text" href="https://github.com/${data[i].getGithub()}">Github Profile</a>
+    </div>
+    </div>
+  </div>
+        `
     }
-}
+}}
